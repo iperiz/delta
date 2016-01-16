7c9298effaa5b107a12a4f25d4136cbdce00376b
ivan <ivan@bigone>
2016-01-16 17:55:20 +0100

    Afegit directori test i classe per prova

---
+++ src/calc.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calc
+ *
+ * @author ivan
+ */
+class Calc {
+
+    // use a function without variables
+    function add($a, $b){ 
+        return ($a + $b); 
+    }
+}
